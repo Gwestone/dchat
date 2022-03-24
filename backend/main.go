ff4afb36a07df80175df3f6c8786155cff12f2ac
Gwestonem <gwestonem@gmail.om>
2022-03-24 17:15:43 +0200

    fixed main.go file

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"dchat/backend/DB"
+	_ "dchat/backend/DB"
+	"dchat/backend/auth"
+	"dchat/backend/routes"
+	"dchat/backend/utils"
+	"github.com/gin-gonic/gin"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
+//TODO: upgrade session management system to localCached
 func main() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+	router := gin.Default()
+	router.SetTrustedProxies([]string{})
+
+	db := DB.ConnectDB()
+	env := utils.NewEnv()
+	env.Db = db
+
+	//DB.MigrateUsers(db)
+
+	router.Use(env.SetEnv)
+	authRoute := router.Group("/auth")
+	{
+		authRoute.POST("/login", auth.Login)
+		authRoute.POST("/register", auth.Register)
+	}
+
+	messageRoute := router.Group("/messages")
+	{
+		messageRoute.POST("/all", auth.LoginRequired, routes.Messages)
+		messageRoute.POST("/:receiver", auth.LoginRequired, routes.Message)
+		//messageRoute.POST("/send/:receiver", auth.LoginRequired, routes.Send)
+	}
+
+	err := router.Run(":81")
+	if err != nil {
+		return
+	} // listen and serve on 0.0.0.0:81
 }
